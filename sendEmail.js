775baea20ed05fafc103dd8845451c4dfbc9a20e
unknown <md.shozon041@gmail.com>
2021-07-09 14:59:32 +0600

    test app

--- sendEmail.js
+++ sendEmail.js
@@ -11,7 +11,9 @@ exports.sendEmail = function (cb) {
 	});
 	const mailOptions = {
 		from: '"Sales" <leocollab01@gmail.com>',
-		to: 'lucas@maruboshi.nl',
+		// to: "lucas@maruboshi.nl",
+		to: 'moshiuzzaman@brainstation23.com',
+		// to: 'shozonyrose@gmail.com',
 		subject: 'New Order!!!',
 		text: 'There are some new orders from website.',
 		attachments: [
