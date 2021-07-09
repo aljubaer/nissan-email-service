2533c1844d9900a9a956459c7b3ceb9ab5a048ef
unknown <md.shozon041@gmail.com>
2021-07-09 11:49:49 +0600

    make it better

--- sendEmail.js
+++ sendEmail.js
@@ -1,31 +1,32 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 exports.sendEmail = function (cb) {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.email_user,
-            pass: process.env.email_password,
-        },
-    });
-    const mailOptions = {
-        from: '"Sales" <leocollab01@gmail.com>',
-        to: "lucas@maruboshi.nl",
-        subject: "New Order!!!",
-        text: "There are some new orders from website.",
-        attachments: [
-            {
-                filename: "OrderData.xlsx",
-                path: __dirname + "/OrderData.xlsx",
-            },
-        ],
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            cb(error, null);
-        } else {
-            cb(null, info.response);
-        }
-    });
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: process.env.email_user,
+			pass: process.env.email_password,
+		},
+	});
+	const mailOptions = {
+		from: '"Sales" <leocollab01@gmail.com>',
+		// to: "lucas@maruboshi.nl",
+		to: 'shozonyrose@gmail.com',
+		subject: 'New Order!!!',
+		text: 'There are some new orders from website.',
+		attachments: [
+			{
+				filename: 'OrderData.xlsx',
+				path: __dirname + '/OrderData.xlsx',
+			},
+		],
+	};
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			cb(error, null);
+		} else {
+			cb(null, info.response);
+		}
+	});
 };
