dfb3d9c44a5bdf461d3339c4202dcaae7164657f
aljubaer <bsse0812@iit.du.ac.bd>
2021-11-09 11:34:25 +0600

    adds excel report generator

--- getAdditionalsData.js
+++ getAdditionalsData.js
@@ -1,9 +1,10 @@
+const ExcelJS = require("exceljs");
 const axios = require("axios");
-if (process.env.NODE_ENV === "development") {
-    require("dotenv").config();
-}
+// if (process.env.NODE_ENV === "development") {
+require("dotenv").config();
+// }
 
-exports.generateEmailData = async function () {
+const generateEmailData = async function () {
     let data;
     const params = new URLSearchParams();
     params.set("grant_type", "client_credentials");
@@ -29,18 +30,24 @@ exports.generateEmailData = async function () {
 
     try {
         const unpublishedData = await axios.get("/placed-order", {
-            headers: {
-                "X-Unpublished": "1",
-            },
-            params: {
-                $filter: "data/IsEmailSent/iv eq false",
-            },
+            // headers: {
+            //     "X-Unpublished": "1",
+            // },
+            // params: {
+            //     $filter: "data/IsEmailSent/iv eq false",
+            // },
         });
         data = unpublishedData.data.items;
     } catch (err) {
         console.log("Error to get the place order data");
         data = [];
     }
+
+    const filterDataByDateRange = (date) => {
+        data = data.filter((item) => date > new Date(item.created));
+    };
+
+    filterDataByDateRange(new Date(2021, 7, 1));
 
     const getVehicleData = (id) => {
         return axios.get(`/vehicle/${id}`);
@@ -56,7 +63,8 @@ exports.generateEmailData = async function () {
         return "No vehicle";
     };
 
-    const formatItems = async (items) => items.map((item) => item.data.data.Name.en);
+    const formatItems = async (items) =>
+        items.map((item) => item.data.data.Name.en);
 
     const getFormattedData = async () => {
         let _data = await data.map(async (item) => {
@@ -65,22 +73,23 @@ exports.generateEmailData = async function () {
                 try {
                     vehicleDetails = await getVehicleData(item.data.Vehicle.iv);
                 } catch (error) {
-                    console.log("Error on getting vehicle data");
+                    console.error("Error on getting vehicle data");
                 }
                 try {
                     accessoriesDetails = await getItemsData(item.data.Items.iv);
                 } catch (error) {
-                    console.log("Error on getting accessories data");
+                    console.error("Error on getting accessories data");
                 }
                 try {
                     formattedAccessories = await formatItems(
                         await Promise.all(accessoriesDetails)
-                    )
+                    );
                 } catch (error) {
-                    console.log('Error on formatting accessories');
+                    console.error("Error on formatting accessories");
                     formattedAccessories = [];
                 }
                 return {
+                    date: item.created,
                     name: item.data.Name.iv,
                     location: item.data.LocationName.iv,
                     email: item.data.ContactEmail.iv,
@@ -90,11 +99,12 @@ exports.generateEmailData = async function () {
                 };
             } else {
                 return {
-                    name: '',
-                    location: '',
-                    email: '',
-                    phone: '',
-                    vehicle: '',
+                    date: "",
+                    name: "",
+                    location: "",
+                    email: "",
+                    phone: "",
+                    vehicle: "",
                     items: [],
                 };
             }
@@ -107,45 +117,140 @@ exports.generateEmailData = async function () {
 
     const formattedData = await getFormattedData();
 
-    console.log('Number item fetched ', data.length, new Date());
+    console.log("Number item fetched ", data.length, new Date());
 
     return { rawData: data, formattedData, receivers };
 };
 
-exports.updateData = async (data) => {
-    let res = await data.map(async (item) => {
-        try {
-            let publishedItem = await axios.put(
-                `/placed-order/${item.id}/status/`,
-                {
-                    Status: "Published",
-                },
-                {
-                    headers: {
-                        "X-Unpublished": "1",
-                    },
-                }
-            );
-            const dataToUpdate = {
-                ...publishedItem.data.data,
-                IsEmailSent: {
-                    iv: true,
-                },
-            };
-            let updatedData = await axios.put(
-                `/placed-order/${item.id}/`,
-                dataToUpdate,
-                {
-                    headers: {
-                        "X-Unpublished": "1",
-                    },
-                }
-            );
-            return { status: "success", updatedData };
-        } catch (error) {
-            console.log("Error on updating data!");
-            return { status: "failed", error };
-        }
+generateEmailData()
+    .then(({ formattedData }) => {
+        console.table(formattedData);
+
+        const workbook = new ExcelJS.Workbook();
+
+        workbook.creator = "Abdullah Al Jubaer";
+
+        workbook.calcProperties.fullCalcOnLoad = true;
+
+        workbook.views = [
+            {
+                x: 0,
+                y: 0,
+                width: 10000,
+                height: 20000,
+                firstSheet: 0,
+                activeTab: 1,
+                visibility: "visible",
+            },
+        ];
+
+        const sheet = workbook.addWorksheet("Customer Data");
+        let start = 1;
+
+        // formattedData.forEach((item, index) => {
+        //     sheet.addRows([
+        //         [index + 1, "Date", item.date],
+        //         [index + 1, "Name", item.name],
+        //         [index + 1, "Location", item.location],
+        //         [index + 1, "Email", item.email],
+        //         [index + 1, "Phone", item.phone],
+        //         [index + 1, "Vehicle", item.vehicle],
+        //         [index + 1, "Items", ...item.items],
+        //     ]);
+        //     sheet.mergeCells(`A${start}:A${start + 6}`);
+        //     start = start + 7;
+        // });
+
+        sheet.addRow(["Date", "Name", "Location", "Email", "Phone", "Vehicle", "Items"]);
+
+        formattedData.forEach((item, index) => {
+            sheet.addRow([item.date, item.name, item.location, item.email, item.phone, item.vehicle, ...item.items]);
+        });
+
+        workbook.xlsx.writeFile("OrderDataFormat2.xlsx").then(() => {
+            console.log("File Created");
+        });
+    })
+    .catch((error) => {
+        console.error(error);
     });
-    return await Promise.all(res);
-};
+
+// exports.generateExcel = async (data) => {
+
+//     const { formattedData } = await getFormattedData();
+
+//     const workbook = new ExcelJS.Workbook();
+
+//     workbook.creator = "Abdullah Al Jubaer";
+
+//     workbook.calcProperties.fullCalcOnLoad = true;
+
+//     workbook.views = [
+//         {
+//             x: 0,
+//             y: 0,
+//             width: 10000,
+//             height: 20000,
+//             firstSheet: 0,
+//             activeTab: 1,
+//             visibility: "visible",
+//         },
+//     ];
+
+//     const sheet = workbook.addWorksheet("Customer Data");
+//     let start = 1;
+
+//     formattedData.forEach((item, index) => {
+//         sheet.addRows([
+//             [index + 1, "Name", item.name],
+//             [index + 1, "Location", item.location],
+//             [index + 1, "Email", item.email],
+//             [index + 1, "Vehicle", item.vehicle],
+//             [index + 1, "Items", ...item.items],
+//         ]);
+//         sheet.mergeCells(`A${start}:A${start + 4}`);
+//         start = start + 5;
+//     });
+
+//     workbook.xlsx.writeFile("OrderData.xlsx").then(() => {
+//         console.log("File Created");
+//     });
+// };
+
+// exports.updateData = async (data) => {
+//     let res = await data.map(async (item) => {
+//         try {
+//             let publishedItem = await axios.put(
+//                 `/placed-order/${item.id}/status/`,
+//                 {
+//                     Status: "Published",
+//                 },
+//                 {
+//                     headers: {
+//                         "X-Unpublished": "1",
+//                     },
+//                 }
+//             );
+//             const dataToUpdate = {
+//                 ...publishedItem.data.data,
+//                 IsEmailSent: {
+//                     iv: true,
+//                 },
+//             };
+//             let updatedData = await axios.put(
+//                 `/placed-order/${item.id}/`,
+//                 dataToUpdate,
+//                 {
+//                     headers: {
+//                         "X-Unpublished": "1",
+//                     },
+//                 }
+//             );
+//             return { status: "success", updatedData };
+//         } catch (error) {
+//             console.log("Error on updating data!");
+//             return { status: "failed", error };
+//         }
+//     });
+//     return await Promise.all(res);
+// };
